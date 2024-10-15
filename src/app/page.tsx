eeceb078c1843198486d32d863e00ba604d30433
shota <shouta160618@gmail.com>
2024-10-15 10:27:47 -0500

    add dashbord page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { currentUser } from "@clerk/nextjs/server";
 import SignInPage from "./pages/SignIn";
 import Dashbord from "./pages/dashbord";
+import { Login } from "./supabase/user/login";
 
 export default async function Home() {
-	const user = await currentUser();
-
-	if (user) {
+	const authUser = await currentUser();
+	if (authUser) {
+		const user = await Login(authUser.id);
 		console.log(user); // 認証されたユーザーの情報を確認
 		return <Dashbord user={user} />;
 	}
+
 	return <SignInPage />;
 }
